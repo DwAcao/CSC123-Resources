ea7a89b397588d6d881b493df4ef9a2aac5894c8
Usman Aslam <usman@usman.uk>
2023-03-06 19:57:40 -0800

    Made changes to UI Test class

--- CSC-123-Resources/Week7/UITest.java
+++ CSC-123-Resources/Week7/UITest.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 public class UITest {
 
-	public static final String[] menuOptions = { "Open Checking Account%n","Open Saving Account%n","Open Credit Card Account%n", "List Accounts%n","View Statement%n", "Deposit Funds%n", "Withdraw Funds%n",
-			"Close an Account%n", "Exit%n%n" };
+	public static final String[] menuOptions = { "Option 1%n","Option2%n","Execute all options%n" };
 	
 	public static final String INPUT_PROMPT = "Enter choice";
 	
@@ -13,6 +12,8 @@ public class UITest {
 		
 			UIManager ui=new UIManager(System.in, System.out, menuOptions, INPUT_PROMPT);
 			int option=ui.getMainOption();
+			double d=ui.readDouble("Enter amount of money you need: ");
+			int i=ui.readInt("Enter your age: ");
 		
 
 	}
