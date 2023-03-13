50472fd2896298470b643f545d911c77faff85de
Usman Aslam <usman@usman.uk>
2023-03-13 13:02:18 -0700

    Week8 resources

---
+++ CSC-123-Resources/Week8/ObjectSerializationDemo2.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class ObjectSerializationDemo2 {
+
+	static final int HEAD_OF_THE_QUEUE=0;
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
+
+		ArrayList<Integer> myQueue;
+		
+		File inFile=new File("/Users/usman/tmp/object_store.dat");
+		
+		if(!inFile.exists()) {
+			System.out.println("File does not exist");
+			System.exit(0);
+		}
+
+			
+		FileInputStream fin=new FileInputStream(inFile);
+		ObjectInputStream oin=new ObjectInputStream(fin);
+		
+		Object o = oin.readObject();
+		myQueue=(ArrayList<Integer>)o;
+		
+		while(!myQueue.isEmpty()) {
+			System.out.println(myQueue.remove(HEAD_OF_THE_QUEUE));
+		}
+		
+	}
+}
---
+++ CSC-123-Resources/Week8/StackDemo.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class StackDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		Stack<String> stack1=new Stack<String>();
+		
+		stack1.push("Line 1");
+		stack1.push("Value 2");
+		
+		System.out.println("\n\n-------Example 1");
+		
+		System.out.println(stack1.pop());
+		System.out.println(stack1.pop());
+		
+		
+		System.out.println("\n\n-------Example 2");
+		
+		Stack<Integer> stack2=new Stack<Integer>();
+		stack2.push(1);
+		stack2.push(2);
+		stack2.push(3);
+		stack2.push(4);
+		stack2.push(5);
+
+		System.out.println("Peeking :" + stack2.peek());
+		
+		
+		while(!stack2.empty()) {
+			System.out.println("Poppping :" + stack2.pop());
+		}
+		
+		
+		
+		
+		
+	}
+
+}
